bbb064fea06b8a91e9afcee50a5ad3f80b49efe5
Tyler Ramage <tysamigo@gmail.com>
2020-07-12 22:12:10 -0400

    add comments

--- point.cs
+++ point.cs
@@ -13,11 +13,14 @@ namespace DefenseGame
             Y = y;
         }
         
+        // This calculates the distance between the Point object (the DistanceTo Method is being called on)
+        // and the X and Y coordinates being passed in.
         public int DistanceTo(int x, int y)
         {
             return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
         }
         
+        // This calculates the distance between the two Point objects
         public int DistanceTo(Point point)
         {
             return DistanceTo(point.X, point.Y);
