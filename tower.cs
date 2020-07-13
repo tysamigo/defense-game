91e6ca35bc8343b000f95a8e1703732a879025e6
Tyler Ramage <tysamigo@gmail.com>
2020-07-13 17:26:01 -0400

    add comments

--- tower.cs
+++ tower.cs
@@ -4,24 +4,27 @@ namespace DefenseGame
 {
     class Tower
     {
-        private const int _range = 1;
-        private const int _power = 1;
-        private const double _accuracy = .75;
+        private const int _range = 1; // how far the tower can shoot
+        private const int _power = 1; // how much to decrease the invader's health
+        private const double _accuracy = .75; // how accurate the tower is at successfully shooting an invader
         
-        private static readonly Random _random = new Random();
+        private static readonly Random _random = new Random(); // allows the tower to miss its target by using the Random Class to generate a random number
         
         private readonly MapLocation _location;
         
-        public Tower(MapLocation location)
+        public Tower(MapLocation location)  // Initialize the tower's location on the map
         {
             _location = location;
         }
         
+        // Determines if the tower hits its target by comparing the 
+        // randomly generated number with the accuracy of the tower.
         private bool IsSuccessfulShot()
         {
             return _random.NextDouble() < _accuracy;
         }
         
+        // Loops through the invaders array and shoots at them
         public void FireOnInvaders(Invader[] invaders)
         {
             foreach(Invader invader in invaders)
