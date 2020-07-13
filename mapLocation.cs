5d698bc08e04fc8db70126588e8d58dfceeb6403
Tyler Ramage <tysamigo@gmail.com>
2020-07-12 22:22:17 -0400

    add comments

--- mapLocation.cs
+++ mapLocation.cs
@@ -2,6 +2,7 @@ namespace DefenseGame
 {
     class MapLocation : Point
     {
+        // Verifies if the Maplocation being constructed is actually on the map
         public MapLocation(int x, int y, Map map) : base(x, y)
         {
             if (!map.OnMap(this))
@@ -10,6 +11,7 @@ namespace DefenseGame
             }
         }
         
+        // Determines if this MapLocation Object is in range of the MapLocation Object being passed in
         public bool InRangeOf(MapLocation location, int range)
         {
             return DistanceTo(location) <= range;
