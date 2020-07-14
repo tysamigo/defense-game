ee56e8338f827ccf1b86f66140c1416376511a1f
Tyler Ramage <tysamigo@gmail.com>
2020-07-14 16:54:36 -0400

    add sniperTower.cs

---
+++ sniperTower.cs
@@ -0,0 +1,12 @@
+namespace DefenseGame
+{
+    class SniperTower : Tower
+    {
+        protected override int Range { get; } = 2;
+        
+        protected override double Accuracy { get; } = 1.0;
+        
+        public SniperTower(MapLocation location) : base(location)
+        {}
+    }
+}
