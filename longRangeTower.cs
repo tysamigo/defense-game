14efa4faf8d55357ce8baec11e5a234ad97dcc66
Tyler Ramage <tysamigo@gmail.com>
2020-07-14 16:56:33 -0400

    add longRangeTower.cs

---
+++ longRangeTower.cs
@@ -0,0 +1,10 @@
+namespace DefenseGame
+{
+    class LongRangeTower : Tower
+    {
+        protected override int Range { get; } = 3;
+        
+        public LongRangeTower(MapLocation location) : base(location)
+        {}
+    }
+}
