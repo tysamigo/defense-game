2456c3c71310bfab3e73231379936f7cce5eb958
Tyler Ramage <tysamigo@gmail.com>
2020-07-15 21:46:03 -0400

    add comments

--- point.cs
+++ point.cs
@@ -13,6 +13,23 @@ namespace DefenseGame
             Y = y;
         }
         
+        public override string ToString()
+        {
+            return X + "," + Y;
+        }
+
+        public override bool Equals(object obj)  // this overrides the Equals method to return true if the coordinates of the two points are equal
+        {
+            if(!(obj is Point))
+            {
+                return false;
+            }
+
+            Point that = obj as Point;  // casts obj to a Point
+
+            return this.X == that.X && this.Y == that.Y;
+        }
+        
         // This calculates the distance between the Point object (the DistanceTo Method is being called on)
         // and the X and Y coordinates being passed in.
         public int DistanceTo(int x, int y)
