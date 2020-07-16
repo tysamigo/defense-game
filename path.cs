6796bf6fc381ca5c0b7374bb8568f5d596fecaed
Tyler Ramage <tysamigo@gmail.com>
2020-07-15 21:45:33 -0400

    add IsOnPath method

--- path.cs
+++ path.cs
@@ -15,5 +15,17 @@ namespace DefenseGame
         {
             return (pathStep < _path.Length) ? _path[pathStep] : null;
         }
+
+        public bool IsOnPath (MapLocation location)
+        {
+            foreach(var pathLocation in _path)
+            {
+                if(location.Equals(pathLocation))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
